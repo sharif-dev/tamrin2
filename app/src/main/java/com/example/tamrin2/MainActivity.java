d2e99acfa1fe65d13c110cac171235f036795557
baharkhd <baharkh127@gmail.com>
2020-04-21 19:40:58 +0430

    play sound on alarm and stops after specific time

--- app/src/main/java/com/example/tamrin2/MainActivity.java
+++ app/src/main/java/com/example/tamrin2/MainActivity.java
@@ -6,7 +6,11 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,11 +19,13 @@ import android.widget.Toast;
 import com.example.tamrin2.alarmFeature.AlarmReceiver;
 import com.example.tamrin2.alarmFeature.AlarmService;
 
+import java.io.IOException;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private static Context context;
+    private static MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.set_alarm_activity);
 
         context = getApplicationContext();
+        player = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+
+//
+//        MediaPlayer player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+//        player.setLooping(true);
+//        player.start();
 
 //        Intent intent = new Intent(this, AlarmService.class);
 //        startService(intent);
@@ -38,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
 //        intent.putExtra("minute", minute);
 //        intent.putExtra("second", second);
 //        startService(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        System.out.println("helloooooo");
+        super.onResume();
     }
 
     public void setAlarm(View view) throws InterruptedException {
@@ -60,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         Intent intent = new Intent(getApplicationContext(), AlarmReceiver.class);
+        intent.setAction("start service");
         PendingIntent pendingIntent = PendingIntent.getBroadcast(
                 getApplicationContext(), 234324243, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
@@ -77,5 +96,9 @@ public class MainActivity extends AppCompatActivity {
 
     public static void setContext(Context context) {
         MainActivity.context = context;
+    }
+
+    public static MediaPlayer getPlayer() {
+        return player;
     }
 }
--- app/src/main/java/com/example/tamrin2/alarmFeature/AlarmReceiver.java
+++ app/src/main/java/com/example/tamrin2/alarmFeature/AlarmReceiver.java
@@ -1,23 +1,66 @@
 package com.example.tamrin2.alarmFeature;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.provider.Settings;
 import android.widget.Toast;
 
 import com.example.tamrin2.MainActivity;
 
+import static android.content.Context.ALARM_SERVICE;
+
 public class AlarmReceiver extends BroadcastReceiver {
+
+    MediaPlayer player = MainActivity.getPlayer();
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Toast.makeText(context, "Alarm....", Toast.LENGTH_LONG).show();
+        String action = intent.getAction();
 
-        Intent n = new Intent(context, AlarmService.class);
-        n.putExtra("uri", intent.getExtras().getString("uri"));
-        context.startService(n);
+//        player = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
 
+        switch (action){
+            case "start service":
+                Toast.makeText(context, "Alarm....", Toast.LENGTH_LONG).show();
+
+
+                player.setLooping(true);
+                player.start();
+//                Intent n = new Intent(context, AlarmService.class);
+//                n.putExtra("uri", intent.getExtras().getString("uri"));
+//                context.startService(n);
+
+
+
+                setStopAlarmManager(context);
+                break;
+            case "stop sound":
+                try {
+                    player.stop();
+                    System.out.println("************** ALOOOOOOOOO");
+                } catch (Exception e) {
+                    System.out.println("&&&& " + e.getMessage());
+                }
+
+                break;
+
+        }
+
+    }
+
+    public void setStopAlarmManager(Context context) {
+        Intent intent = new Intent(context.getApplicationContext(), AlarmReceiver.class);
+        intent.setAction("stop sound");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                context.getApplicationContext(), 223423423, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                + (10 * 1000), pendingIntent);
+        System.out.println("OY KKKKKKKKKKKKKKKKKKKKKKK");
     }
 }
