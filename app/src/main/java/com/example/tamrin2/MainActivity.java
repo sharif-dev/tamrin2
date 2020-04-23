618aa88ee865431d74519a79b2aa9fef973cc413
ghshenavar <ghshenavar@gmail.com>
2020-04-23 20:55:54 +0430

    view almost finished

--- app/src/main/java/com/example/tamrin2/MainActivity.java
+++ app/src/main/java/com/example/tamrin2/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.tamrin2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SeekBar sBar;
+    private TextView tView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sBar = findViewById(R.id.degrees);
+        tView =  findViewById(R.id.textView);
+        tView.setText(R.string.degree);
+        sBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int pval = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                pval = progress;
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //write custom code to on start progress
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //...
+            }
+        });
     }
 }
