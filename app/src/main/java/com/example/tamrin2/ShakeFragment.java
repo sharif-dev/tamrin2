f80cc563967418b5cb9e1ca054a6db221b49c936
AmirHossein <amiryousefi98@gmail.com>
2020-04-24 19:42:06 +0430

    second feature fragment

---
+++ app/src/main/java/com/example/tamrin2/ShakeFragment.java
@@ -0,0 +1,45 @@
+package com.example.tamrin2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ToggleButton;
+import java.util.Objects;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ShakeFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View shakeView =inflater.inflate(R.layout.shake_detector_activity , container, false);
+
+         final ToggleButton activated = (ToggleButton) shakeView.findViewById(R.id.active_btn);
+        activated.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (activated.isChecked()){
+                    initService();
+                }
+                else {
+                    killService();
+                }
+            }
+        });
+        return shakeView;
+    }
+    private void initService(){
+        Log.d(getClass().getSimpleName() , "initservice Function called");
+        Objects.requireNonNull(getActivity()).startService(new Intent(getActivity() , MySensorService.class));
+    }
+    private void killService(){
+        Log.d(getClass().getSimpleName() , "killserver Function called");
+        Objects.requireNonNull(getActivity()).stopService(new Intent(getActivity() , MySensorService.class));
+    }
+}
