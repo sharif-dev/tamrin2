9a6a30835929cff6c928c2eee3a404778434510e
AmirHossein <amiryousefi98@gmail.com>
2020-05-05 11:10:22 +0430

    MainActivity

--- app/src/main/java/com/example/tamrin2/MainActivity.java
+++ app/src/main/java/com/example/tamrin2/MainActivity.java
@@ -1,36 +1,34 @@
 package com.example.tamrin2;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.app.Notification;
-import android.content.Intent;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.tamrin2.shake.ShakeFragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 public class MainActivity extends AppCompatActivity {
     Button shakeBtn;
+    ShakeFragment shakeFragment;
+    public static DevicePolicyManager deviceManger ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        shakeBtn = (Button) findViewById(R.id.shake_btn);
-        shakeBtn.setVisibility(View.VISIBLE);
-        shakeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                shakeBtn.setVisibility(View.INVISIBLE);
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                ShakeFragment shakeFragment = new ShakeFragment();
-                transaction.add(R.id.container  ,shakeFragment );
-                transaction.addToBackStack(null);
-                transaction.commit();
-            }
-        });
+
+
 
     }
-
+    
+    @Override
+    public void onBackPressed() {
+        shakeBtn.setVisibility(View.VISIBLE);
+        super.onBackPressed();
+    }
 }
